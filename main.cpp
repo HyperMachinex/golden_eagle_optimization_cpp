916de79bbac6dfee39bd9cbe511f25f924c49801
Hyper <armagan.trs@protonmail.ch>
2024-12-26 17:24:34 +0300

    Added core info.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include <numeric>
 #include "GEO.cpp"
 #include <chrono>
-
+#include <windows.h>
+#include <malloc.h>    
+#include <stdio.h>
+#include <tchar.h>
 using namespace std;
 typedef vector<double> Vec;
 
 #define SELECT_ACKLEY 1
-#define SELECT_DIXONPRICE 0 
+#define SELECT_DIXONPRICE 0
 #define SELECT_GRIEWANK 0
 #define SELECT_PERM 0
 #define SELECT_RASTRIGIN 0
@@ -17,15 +20,18 @@ typedef vector<double> Vec;
 #define SELECT_SPHERE 0
 #define SELECT_ZAKHAROV 0
 
-int populationsize = 100;
+int populationsize = 1000;
 int Iteration = 1000;
 int dim = 30;
+//const auto processor_count = thread::hardware_concurrency();
 using std::chrono::high_resolution_clock;
 using std::chrono::duration_cast;
 using std::chrono::duration;
 using std::chrono::milliseconds;
+
 #if SELECT_ACKLEY
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     Vec lb(dim, -32768.0);
     Vec ub(dim, 32768.0);
     TestFunction fun = selectFunction(0);
@@ -41,6 +47,7 @@ int main() {
 
 #if SELECT_DIXONPRICE
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     int dim = 5;
     Vec lb(dim, -10.0);
     Vec ub(dim, 10.0);
@@ -58,6 +65,7 @@ int main() {
 
 #if SELECT_GRIEWANK
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     Vec lb(dim, -600.0);
     Vec ub(dim, 600.0);
     TestFunction fun = selectFunction(2);
@@ -74,6 +82,7 @@ int main() {
 
 #if SELECT_PERM
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     Vec lb(dim, -5.0);
     Vec ub(dim, 5.0);
     TestFunction fun = selectFunction(3);
@@ -90,6 +99,7 @@ int main() {
 
 #if SELECT_RASTRIGIN
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     Vec lb(dim, -5.12);
     Vec ub(dim, 5.12);
     TestFunction fun = selectFunction(4);
@@ -106,6 +116,7 @@ int main() {
 
 #if SELECT_ROSENBROCK
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     Vec lb(dim, -2048.0);
     Vec ub(dim, 2048.0);
     TestFunction fun = selectFunction(5);
@@ -122,6 +133,7 @@ int main() {
 
 #if SELECT_SCHWEFEL
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     Vec lb(dim, -500.0);
     Vec ub(dim, 500.0);
     TestFunction fun = selectFunction(6);
@@ -138,6 +150,7 @@ int main() {
 
 #if SELECT_SPHERE
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     Vec lb(dim, -5.12);
     Vec ub(dim, 5.12);
     TestFunction fun = selectFunction(7);
@@ -154,6 +167,7 @@ int main() {
 
 #if SELECT_ZAKHAROV
 int main() {
+    printf("Running on %d cores.\n", atoi(getenv("NUMBER_OF_PROCESSORS")));
     int dim = 5;
     Vec lb(dim, -5.0);
     Vec ub(dim, 10.0);
